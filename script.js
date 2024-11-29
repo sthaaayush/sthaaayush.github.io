6dc051e3d177ff84f894d09b4c21288278d4a353
Aayush Shrestha <aayushstha20@gmail.com>
2024-11-29 21:25:41 +0545

    configure certificate and responsive navbar

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function downloadCV() {
-    const link = document.createElement('a');
-    link.href = 'Aayush_CV.pdf'; // Replace with the correct path to your CV
-    link.download = 'Aayush_CV.pdf';
-    link.click();
+  const link = document.createElement("a");
+  link.href = "Aayush_CV.pdf";
+  link.download = "Aayush_CV.pdf";
+  link.click();
 }
+
+// Get references to the menu button and the menu links
+const menuButton = document.getElementById("menuButton");
+const mobileMenu = document.getElementById("mobileMenu");
+
+// Toggle the mobile menu visibility
+menuButton.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
